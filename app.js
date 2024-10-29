7442ab9ec1ee5ff6399d767082c6bf4ad095014b
jrehlaender91 <73409365+jrehlaender91@users.noreply.github.com>
2024-10-28 21:34:17 -0600

    Final version
    Fully functional

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 const qwerty = document.querySelector("#qwerty");
 const phrase = document.querySelector("#phrase");
-const startButton = document.querySelector(".btn__reset");
+const startButton = document.querySelector(".btn__start");
+const resetButton = document.querySelector(".btn__reset");
 const list = document.querySelector("#phrase ul");
 const hearts = document.querySelector("#scoreboard ol");
-
+const keyrows = document.querySelector(".keyrow");
+const heading = document.querySelector("h2");
+let phraseArray = "";
+let overlay = document.querySelector("#overlay");
 
 let missed = 0;
+let letterFound = null; 
 
 const phrases = [
-    "You must be the change you wish to see in the world", 
-    "It is during our darkest moments that we must focus to see the light",
-    "The way to get started is to quit talking and begin doing",
-    "Do one thing every day that scares you",
-    "The only thing we have to fear is fear itself" 
+    "Arctic Monkeys", 
+    "Radiohead",
+    "Pulp",
+    "Pink Floyd",
+    "The Beatles",
+    "Blur",
+    "David Bowie",
+    "Joy Division",
+    "Sex Pistols",
+    "Queen"
 ];
 
-// Event listener for the start button.
-//      Hides the overlay div.
-startButton.addEventListener("click", (e) => {
-    let overlay = document.querySelector("#overlay");
-    overlay.style.display = "none";
-});
+
+
 
 // Function that returns a random phrase from an array.
 function getRandomPhraseAsArray(arr){
     let randomNumber = Math.floor(Math.random() * 6);
     return arr[randomNumber];
 } 
-
 
 
 // Function that adds phrase to the display.
@@ -42,107 +47,111 @@ function addPhraseToDisplay(arr) {
         } else {
             node.className = "letter";
             list.appendChild(node);
-        }
-        
+        } 
     }
 }
 
-const phraseArray = getRandomPhraseAsArray(phrases);
-console.log(phraseArray);
-addPhraseToDisplay(phraseArray);
-
-const checkLetter = button => {
+// Function checks if letter is in the array and shows it.
+const checkLetter = (button) => {
     let item = list.children;
-       
+    let letter = "";
+    letterFound = null;
+
     for(let i = 0; i < item.length; i++) {
-        let letter = item[i].textContent;
-        if(button === letter) {
-            console.log("match");
-            let letterFound = button;
+        letter = item[i].textContent;
+        
+        if(button.toUpperCase() === letter.toUpperCase()) {
             item[i].className = item[i].className + " show";
-            return letterFound;    
-        } else {
-            return null;      /* CHECK HERE: This function returns always null*/
-        }
-     } 
+            item[i].style.transition = "all 2s";
+            letterFound = button;
+        } 
+    } 
+    return letterFound;
 };
 
+// Event listener for the keyboard that calls the checkLetter function.
 qwerty.addEventListener("click", e => {
     e.preventDefault();
     if(e.target.tagName === 'BUTTON') {
         let button = e.target.textContent;
         e.target.disabled = true;  
         e.target.className = 'chosen';
-        console.log(checkLetter(button));  
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-qwerty.addEventListener("click", (e) => {
-        let letter = e.target.textContent;
-        e.target.className = "chosen";
-        e.target.disabled = "true";
-        
-        if (checkLetter(letter) === null) {
-            let heartLi = hearts.children;
-            let heartImg = document.querySelector("#scoreboard ol li img");
-            console.log(heartImg);
-            console.log(missed);
-            heartImg.src = "images/lostHeart.png";
-            missed += 1;
-            
-            
-
-        }
-        
-});
     
+        checkLetter(button); 
+        
+        
+        if(letterFound === null) {
+            let score = document.querySelector("#scoreboard ol");
+            let hearts = score.children
+            hearts[missed].getElementsByTagName("IMG")[0].src = "images/lostHeart.png";
+            missed += 1;
+        }
+    }
+    checkWin();
+});
 
 
-let checkLetter = (letter) => {
+const checkWin = () => {
     let item = list.children;
-    let letterFound = letter;
-    let letterMatched = "";
+    let showCount = 0;
+    let letterCount = 0;
 
-    for(let i = 0; i < item.length; i++) {
-        if (item[i].textContent === letterFound) {
-            item[i].className = item[i].className + " show";
-            letterMatched = letterFound;
-            return letterMatched;  
+    for(let i = 0; i < item.length; i++) {       
+        if(item[i].className.includes('letter')) {
+            letterCount += 1;
         } 
-    } return null;
-}; */
+
+        if (item[i].className.includes('show')) {
+            showCount += 1;
+        }
+    }
+
+    if(letterCount === showCount) {
+        overlay.style.display = "flex";
+        overlay.className = "win"
+        heading.textContent = "You won!"
+        startButton.style.display = "none";
+        resetButton.style.display = "inline";
+    } else if (missed === 5) {
+        overlay.style.display = "flex";
+        overlay.className = "lose"
+        heading.textContent = "Try again!"
+        startButton.style.display = "none";
+        resetButton.style.display = "inline";
+    }
+};
+
+// Event listener for the start button.
+//      Hides the overlay div.
+startButton.addEventListener("click", (e) => {
+    overlay.style.display = "none";
+    phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray);
+});
+
+resetButton.addEventListener("click", (e) => {
+    let qwertyRows = qwerty.children;   
+    let allItems = list.children;
+    missed = 0; 
+
+    for(let i = 0; i < qwertyRows.length; i++) {
+        let qwertyButtons = qwertyRows[i].children; 
+        for(let i = 0; i < qwertyButtons.length; i++) {
+            qwertyButtons[i].className = "";
+            qwertyButtons[i].disabled = false;
+        }
+    }
+
+    for(let i = 0; i < 5; i++) {
+        let score = document.querySelector("#scoreboard ol");
+        let hearts = score.children;
+        hearts[i].getElementsByTagName("IMG")[0].src = "images/liveHeart.png";
+    }
+
+    list.innerHTML = '';
+    phraseArray = getRandomPhraseAsArray(phrases);
+    console.log(phraseArray);
+    addPhraseToDisplay(phraseArray);
+    overlay.style.display = "none";
+
+});
